03271333d7ed5c844118c06ddf0fb8398dbdb81b
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-03-03 20:47:03 +0100

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Vladyslav Dikhtiaruk on 03/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
